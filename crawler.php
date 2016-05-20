8e525e5cd3a343ed7110d901b79b99a26da3da64
Davis.Du <davis.du@bluecomgroup.com>
2016-05-20 15:22:07 +0800

    add new file

---
+++ crawler.php
@@ -0,0 +1,18 @@
+<?php
+use Symfony\Component\DomCrawler\Crawler;
+require 'Bootstrap.php';
+$html = <<<'HTML'
+<!DOCTYPE html>
+<html>
+    <body>
+        <p class="message">Hello World!</p>
+        <p>Hello Crawler!</p>
+    </body>
+</html>
+HTML;
+
+$crawler = new Crawler($html);
+
+foreach ($crawler as $domElement) {
+    var_dump($domElement->nodeName);
+}
